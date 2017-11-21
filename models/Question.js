0182dae689f5fa24564c8e676208afd0c5dc3fef
Alice Freda <alicefreda@gmail.com>
2017-11-21 01:13:13 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,22 @@
 const db = require("../config/db")
 
 class Question{
+  static CreateTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS questions (
+        id INTEGER PRIMARY KEY,
+        question TEXT
+      )
+    `
 
+    console.log("Preparing to create the questions table...")
+
+    return new Promise(function(resolve){
+      db.run(sql, function(){
+        console.log("...questions table created!")
+        resolve("Success")
+      })
+    })
 }
 
 
